ecef1f3c33cc72a951fab4f111d3aafe3566ba41
ajie <11jkyj@sina.com>
2019-09-25 20:37:28 +0800

    test 默认加载器的加载路径

---
+++ src/jvm/classloaderProcessed/LoaderTest8.java
@@ -0,0 +1,35 @@
+package jvm.classloaderProcessed;
+
+import org.junit.Test;
+
+/**
+ * @author: wyj
+ * @date: 2019/9/25
+ * @description:
+ */
+public class LoaderTest8 {
+    // 打印三种系统默认类加载的加载路径；
+    public static void main(String[] args){
+
+        System.out.println(System.getProperty("sun.boot.class.path"));
+        System.out.println(System.getProperty("java.ext.dirs"));
+        System.out.println(System.getProperty("java.class.path"));
+    }
+
+    @Test
+    public void test001() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        LoaderTest5 myLoader1 = new LoaderTest5("loader1");
+        Class<?> aClass = myLoader1.loadClass("beans.User");
+        System.out.println(aClass.getClassLoader());
+        System.out.println(aClass.hashCode());
+        Object object = aClass.newInstance();
+
+        /**
+         * 其中根类加载的路径有C:\Program Files\Java\jdk1.8.0_162\jre\classes
+         *  那么把User.class放到上面的目录，是不是就是由根类加载器来加载这个类的？
+         *  执行结果：null
+                      2027961269
+           确实是由根类加载器加载的。。。
+         */
+    }
+}
